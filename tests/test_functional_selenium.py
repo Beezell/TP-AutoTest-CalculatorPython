5578a2b45f9a47e92b7fd121b94703b09f9102ab
Beezell <abigael.castillo@live.fr>
2025-06-20 11:36:04 +0200

    seleniumSansInterface

--- tests/test_functional_selenium.py
+++ tests/test_functional_selenium.py
@@ -2,6 +2,7 @@ import sys
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 def test_addition():
@@ -11,7 +12,12 @@ def test_addition():
     else:
         service = Service("drivers/chromedriver")  # version Linux, sans .exe
 
-    driver = webdriver.Chrome(service=service)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # mode sans interface graphique
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     driver.get("http://localhost:5000")
 
